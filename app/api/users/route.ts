3beb8636022c738aa153568f2f28628dc1cf5cdb
withme <168322939+devswithme@users.noreply.github.com>
2024-11-07 09:38:27 +0800

    add ts expect error

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,16 +3,13 @@ import db from '@/lib/db'
 
 export async function POST(req: Request) {
   try {
-    // Ensure the request body is parsed as JSON
     const data = await req.json()
     
-    // Creating a new user entry
     const result = await db.user.create({
       data,
       select: { id: true, nama: true, ibadah: true },
     })
 
-    // Update the newly created user with a link
     await db.user.update({
       where: { id: result.id },
       data: {
@@ -20,19 +17,17 @@ export async function POST(req: Request) {
       },
     })
 
-    // Prepare the dynamic property name for quota based on the user's ibadah
     const propQuota = `KU${result.ibadah}` as keyof typeof db.quota
     const quota = await db.quota.findMany()
 
-    // Dynamically update the quota
     await db.quota.update({
       where: { id: 1 },
       data: {
+        // @ts-expect-error test
         [propQuota]: quota[0][propQuota] - 1,
       },
     })
 
-    // Return the result in the response
     const response = NextResponse.json(result, { status: 201 })
     response.headers.set('Access-Control-Allow-Origin', '*') // Adjust origin as needed
 
